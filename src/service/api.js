9d6d81a216798cb20007f15742bd81e453d2aa74
snowmaner <1027092645@qq.com>
2018-12-01 15:38:57 +0800

    a

--- src/service/api.js
+++ src/service/api.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 // axios.defaults.baseURL = 'https://www.easy-mock.com/mock/5baca0db71ecff25e3cf5a04/house'
 
 export function api(){//发送请求
-  return axios.get('/static/mock/city.json')// /home 
+  // return axios.get('/static/mock/city.json')// /home 
+  return axios.get('/dist/static/mock/city.json')
 }
 
